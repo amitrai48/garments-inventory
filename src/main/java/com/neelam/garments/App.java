1dbb5597b077030b1964e61e8bef3b24f7ccbf11
amitrai48 <amitrai48@gmail.com>
2016-05-06 18:42:35 +0530

    initial boiler plate

---
+++ src/main/java/com/neelam/garments/App.java
@@ -0,0 +1,13 @@
+package com.neelam.garments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
